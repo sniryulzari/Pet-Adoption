a0a2a8e50c0d64b1d255925d9bdd200d308b08ca
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2023-03-06 11:39:32 +0200

    mongo connect

--- fs-pet-adoption-be-sniryulzari/server.js
+++ fs-pet-adoption-be-sniryulzari/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config({path: "./.env"});
+require("dotenv").config({ path: "./.env" });
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-const app =  express();
+const app = express();
 
-app.use("/images", express.static('Images'));
-app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+app.use("/images", express.static("Images"));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -17,28 +17,28 @@ const PORT = process.env.PORT || 8080;
 const usersRoute = require("./Routes/UsersRoute");
 const petsRoute = require("./Routes/PetsRoute");
 const adminRoute = require("./Routes/AdminRoute");
-const appOperationsRoute= require("./Routes/AppOperationsRoute")
+const appOperationsRoute = require("./Routes/AppOperationsRoute");
 
-
-app.use("/users",  usersRoute);
+app.use("/users", usersRoute);
 app.use("/pets", petsRoute);
 app.use("/admin", adminRoute);
 app.use("/appOperations", appOperationsRoute);
 
 mongoose.set("strictQuery", false);
 
-const connect = mongoose.connect(
-  process.env.MONGO_URI,
-  {useNewUrlParser: true, useUnifiedTopology: true}
-)
-connect.then(() => {
-  console.log('Connected to db');
-  app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to db");
+    app.listen(PORT, () => {
+      console.log(`App is listening on port: ${PORT}`);
+    });
+    console.log("mongo uri:", process.env.MONGO_URI);
+  })
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
   });
-}).catch(err => {
-  console.error(err.stack);
-  process.exit(1);
-})
-
-
