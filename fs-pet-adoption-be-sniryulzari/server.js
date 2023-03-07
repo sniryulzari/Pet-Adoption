74b5f614d93168e4ea969cc7446fa7f9c6fbbd25
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2023-03-07 12:39:24 +0200

    Mongo Uri

--- fs-pet-adoption-be-sniryulzari/server.js
+++ fs-pet-adoption-be-sniryulzari/server.js
@@ -25,35 +25,23 @@ app.use("/appOperations", appOperationsRoute);
 
 mongoose.set("strictQuery", true);
 
-// const URI = String(process.env.MONGO_URI);
+let URI = process.env.MONGO_URI;
+let connect = URI.toString();
+console.log("connect:", connect); 
 
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to db");
-//     app.listen(PORT, () => {
-//       console.log(`App is listening on port: ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error(err.stack);
-//     process.exit(1);
-//   });
-
-  mongoose
-  .connect(process.env.MONGO_URI, {
+mongoose
+  .connect(connect, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
+    console.log("Connected to db");
     app.listen(PORT, () => {
-      console.log("Listening on port " + PORT);
+      console.log(`App is listening on port: ${PORT}`);
     });
   })
   .catch((err) => {
     console.error(err.stack);
     process.exit(1);
   });
+  
